7d5815d417aa71697cb5119ba911e44337ce0ec0
Morshed22 <morshedalamustc22@gmail.com>
2025-04-05 13:36:47 -0400

    Initial Commit

---
+++ ShapesDemo/ShapesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesDemoApp.swift
+//  ShapesDemo
+//
+//  Created by Morshed Alam on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ShapesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
